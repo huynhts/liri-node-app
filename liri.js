b02edd98ab1899fe03e7f908717cf56945b03929
winTS <huynhts@gmail.com>
2019-08-02 18:23:09 -0700

    added do what it says functionality

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 // import key.js file and store into variable
 var keys = require("./keys.js");
 var colors = require('colors');
+var ranTxt = require('fs');
 
 
 var liriCommand = process.argv[2];
@@ -11,6 +12,7 @@ var liriSearch = process.argv[3];
 //add switch case function for node commands:
 switch (liriCommand) {
     case 'concert-this':
+        concertTime();
         //add function to search artist/band name; pass it liriSearch variable
         break;
     case 'spotify-this-song':
@@ -22,13 +24,30 @@ switch (liriCommand) {
         //add function to search movie title; pass it liriSearch variable
         break;
     case 'do-what-it-says':
+        randomCommand();
         //add function to read cmd from text
         break;
     default: 
         console.log('Sorry, that command is not available at this time. Please try another command.');
 };
 
-
+//function that looks up concerts and prints information on CML
+function concertTime () {
+    var axios = require('axios');
+    var artist = liriSearch;
+    var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        axios.get(url).then(function(response) {
+            console.log(response);
+            //     "Movie Title: ".bold.green + response.data.Title +
+            //     + "\n Release Year: ".bold.green + response.data.Year 
+            //     + "\n IMDB Rating: ".bold.green + response.data.imdbRating 
+            //     + "\n Production Country: ".bold.green + response.data.Country 
+            //     + "\n Language: ".bold.green + response.data.Language 
+            //     + "\n Plot: ".bold.green + response.data.Plot 
+            //     + "\n Actors/Actresses: ".bold.green + response.data.Actors
+            // );
+        });
+};
 
 //function that looks up movies and prints information on CML
 function movieTime () {
@@ -38,12 +57,12 @@ function movieTime () {
         axios.get(url).then(function(response) {
             console.log(
                 "Movie Title: ".bold.green + response.data.Title +
-                "\n Release Year: ".bold.green + response.data.Year +
-                "\n IMDB Rating: ".bold.green + response.data.imdbRating +
-                "\n Production Country: ".bold.green + response.data.Country +
-                "\n Language: ".bold.green + response.data.Language +
-                "\n Plot: ".bold.green + response.data.Plot + 
-                "\n Actors/Actresses: ".bold.green + response.data.Actors
+                + "\n Release Year: ".bold.green + response.data.Year 
+                + "\n IMDB Rating: ".bold.green + response.data.imdbRating 
+                + "\n Production Country: ".bold.green + response.data.Country 
+                + "\n Language: ".bold.green + response.data.Language 
+                + "\n Plot: ".bold.green + response.data.Plot 
+                + "\n Actors/Actresses: ".bold.green + response.data.Actors
             );
         });
 };
@@ -57,6 +76,13 @@ function spotifyTime () {
     var spotifyKey = new Spotify(keys.spotify);
     var songName = liriSearch;
 
+    if (!liriSearch) {
+        songName = "The Sign";
+    } else {
+        songName = liriSearch;
+    };
+
+
     spotifyKey.search({ type: 'track', query: songName, limit: 10})
     .then(function(response) {
         var spotifyResults = response.tracks.items;
@@ -67,14 +93,15 @@ function spotifyTime () {
                 trackArtist = value.name;
                 totArtists.push(trackArtist);
             });
-            console.log("------Result #" + count + "------" + "\n")
-            console.log("Track: ".bold.red + response.tracks.items[i].name);
-            console.log("Track Artists: ".bold.red + totArtists);
-            console.log("Album: ".bold.red + response.tracks.items[i].album.name);
+            console.log("------Result #" + count + "------" + "\n" 
+                + "Track: ".bold.blue + response.tracks.items[i].name
+                + "\nTrack Artists: ".bold.blue + totArtists 
+                + "\nAlbum: ".bold.blue + response.tracks.items[i].album.name
+            );
             if (response.tracks.items[i].preview_url == null){
-                console.log("Sorry track preview is not available!".bold.red.underline + "\n")
+                console.log("Sorry track preview is not available!".bold.blue.underline + "\n")
             } else {
-                console.log("Preview URL: ".bold.red + response.tracks.items[i].preview_url + "\n");   
+                console.log("Preview URL: ".bold.blue + response.tracks.items[i].preview_url + "\n");   
             };
 
             count++;
@@ -85,8 +112,31 @@ function spotifyTime () {
     });
 }
 
+function randomCommand (){
+    ranTxt.readFile('random.txt', 'utf8', function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        
+        var ranCmd = [];
+        var input = data.split(',');
+        for (i=0; i<input.length; i++){
+            ranCmd.push(input[i])
+        };
 
-// console.log(response.tracks.items[0].album.name);
-// console.log(response.tracks.items[0].artists[0].name);
-// console.log(response.tracks.items[0].name);
-// console.log(response.tracks.items[0].preview_url);
+        var liriCommand = ranCmd[0];
+        var liriSearch = ranCmd[1];
+
+        console.log('I think it worked? ' + liriCommand + " " + liriSearch);
+
+        if(liriCommand === 'concert-this'){
+            concertTime(liriSearch);
+        } else if (liriCommand === 'spotify-this-song'){
+            spotifyTime(liriSearch);
+        } else if (liriCommand === 'movie-this') {
+            movieTime(liriSearch);
+        };
+        
+
+    });
+};
